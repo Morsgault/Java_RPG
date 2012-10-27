0fec7baf4cf70bc2cc505ba7a0835bf35a507385
Jason Holman <laxermaster15@gmail.com>
2012-10-26 21:03:14 -0400

    Made some small changes to the AttackEngine

--- src/AttackEngine.java
+++ src/AttackEngine.java
@@ -1,5 +1,13 @@
 import java.util.Random;
 public class AttackEngine {
+    /*
+     *Weapons are not currently going
+     *to have an effect.
+     *Once the the game is running the
+     *First update will include the
+     *implementation of weapons
+     *
+     */
     private int plevel; //Player Level
     private int elevel; //Enemy Level
     private int P_MAX_DAM; //Maximum Damage a player can do
@@ -7,13 +15,23 @@ public class AttackEngine {
     private int E_MAX_DAM; //Maximum Damage an enemy can do
     private int E_MIN_DAM; //Minumum Damage an enemy can do
     private boolean E_ALIVE; //Enemy is alive
+    private boolean P_ALIVE; //Player is alive
+    private Character player;
+    private Random elgen;
+
 
     public AttackEngine(String charName){
         Character player = new Character(charName);
-        plevel =  player.getLevel();
         Random elgen = new Random();
+    }
+    public void Battle(){
         elevel = elgen.nextInt(2)+(plevel-1);
-
+        while(E_ALIVE==true && P_ALIVE==true){
+            switch(elevel){
+                case -1:
+                //To Finish
+              }
+        }
     }
 }
 
