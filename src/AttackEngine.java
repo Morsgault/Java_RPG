abec57fc4e64aa7615ccbdb91f056b9d61eef985
Vinny Diehl <gbchaosmaster926@gmail.com>
2012-11-07 18:12:04 -0500

    Normalize indentation
    4 spaces seems to be the norm, and Vim plays nicer with 4 space
    indentation, so making it that.

--- src/AttackEngine.java
+++ src/AttackEngine.java
@@ -2,115 +2,115 @@ package txtrpg;
 import java.io.IOException;
 import java.util.Random;
 public class AttackEngine {
-	/*
-	 *Weapons are not currently going
-	 *to have an effect.
-	 *Once the the game is running the
-	 *First update will include the
-	 *implementation of weapons
-	 *
-	 */
-	private int plevel; //Player Level
-	private int elevel; //Enemy Level
-	private boolean E_ALIVE; //Enemy is alive
-	private boolean P_ALIVE; //Player is alive
-	private int E_HEALTH; //Enemy's Max Health
-	private int EC_HEALTH; //Enemy's Current Health
-	private int P_HEALTH; //Player's Health
-	private int PC_HEALTH; //Player's Current Health
-	private final int P_MAX_DAMAGE[] = {40,50,60,70,80,90,100,110,120,130,140,150}; //Array of maximum damages for player
-	private final int E_MAX_DAMAGE[] = {5,20,30,40,50,60,70,80,90,100,110,120};
-	private int choice; //Holder variable for player input
-	private int damage;//The amount of damage done
-	private boolean flee; //Tells if a player flees
-	private Character player;
-	private Random elgen;
-	private Random engine;
+    /*
+     *Weapons are not currently going
+     *to have an effect.
+     *Once the the game is running the
+     *First update will include the
+     *implementation of weapons
+     *
+     */
+    private int plevel; //Player Level
+    private int elevel; //Enemy Level
+    private boolean E_ALIVE; //Enemy is alive
+    private boolean P_ALIVE; //Player is alive
+    private int E_HEALTH; //Enemy's Max Health
+    private int EC_HEALTH; //Enemy's Current Health
+    private int P_HEALTH; //Player's Health
+    private int PC_HEALTH; //Player's Current Health
+    private final int P_MAX_DAMAGE[] = {40,50,60,70,80,90,100,110,120,130,140,150}; //Array of maximum damages for player
+    private final int E_MAX_DAMAGE[] = {5,20,30,40,50,60,70,80,90,100,110,120};
+    private int choice; //Holder variable for player input
+    private int damage;//The amount of damage done
+    private boolean flee; //Tells if a player flees
+    private Character player;
+    private Random elgen;
+    private Random engine;
 
-	public AttackEngine(String charName) throws NumberFormatException, IOException{
-		player = new Character(charName);
-		elgen = new Random();
-	}
-	public boolean Battle() throws IOException{
-		boolean alive = true;
-		int health;
-		flee = false;
-		P_HEALTH = player.getHealth();
-		PC_HEALTH = player.getCHealth();
-		plevel = player.getLevel();
-		elevel = elgen.nextInt(2)+(plevel-1);
-		if(elevel !=-1){
-			health = 70 + (elevel + 1) * 30;
-		}
-		else{
-			health = 70;
-			elevel = 0;
-		}
+    public AttackEngine(String charName) throws NumberFormatException, IOException {
+        player = new Character(charName);
+        elgen = new Random();
+    }
+    public boolean Battle() throws IOException {
+        boolean alive = true;
+        int health;
+        flee = false;
+        P_HEALTH = player.getHealth();
+        PC_HEALTH = player.getCHealth();
+        plevel = player.getLevel();
+        elevel = elgen.nextInt(2)+(plevel-1);
+        if(elevel !=-1) {
+            health = 70 + (elevel + 1) * 30;
+        }
+        else {
+            health = 70;
+            elevel = 0;
+        }
 
-		E_HEALTH = health;
-		EC_HEALTH = health;
+        E_HEALTH = health;
+        EC_HEALTH = health;
 
-		H.pln("You have encountered a level "+elevel+" enemy");
-		while(EC_HEALTH > 0 && PC_HEALTH > 0 && !flee){
-			engine = new Random();
-			H.pln("Your Health: "+PC_HEALTH+"/"+ P_HEALTH);
-			H.pln("Enemy's Health : "+EC_HEALTH+"/"+ E_HEALTH);
-			H.pln("What do you want to do?");
-			H.pln("(1)Attack, (2)Defend, (3)Flee");
-			choice = H.inputInt();
+        H.pln("You have encountered a level "+elevel+" enemy");
+        while(EC_HEALTH > 0 && PC_HEALTH > 0 && !flee) {
+            engine = new Random();
+            H.pln("Your Health: "+PC_HEALTH+"/"+ P_HEALTH);
+            H.pln("Enemy's Health : "+EC_HEALTH+"/"+ E_HEALTH);
+            H.pln("What do you want to do?");
+            H.pln("(1)Attack, (2)Defend, (3)Flee");
+            choice = H.inputInt();
 
-			switch(choice){
-			case 1:
-				if(engine.nextInt(99)+1 <= 95){
-					damage = engine.nextInt(P_MAX_DAMAGE[plevel]);
-					H.pln("Enemy lost "+damage+" health!");
-					EC_HEALTH-=damage;
-				}
-				else{
-					if(plevel<=8){
-						damage = engine.nextInt(P_MAX_DAMAGE[plevel+2]);
-						H.pln("Critical Hit!");
-						H.pln("Enemy lost "+damage+" health!");
-					}
-					else{
-						if(plevel == 9){
-							damage = engine.nextInt(P_MAX_DAMAGE[plevel+1]);
-							H.pln("Critical Hit!");
-							H.pln("Enemy lost "+damage+" health!");
-						}
-						if(plevel == 10){
-							damage = EC_HEALTH;
-							H.pln("You destroyed the Enemy with one blow!!!");
-						}
-					}
-				}
-				damage = engine.nextInt(E_MAX_DAMAGE[elevel])+1;
-				H.pln("You lost "+damage+" health");
-				PC_HEALTH-=damage;
-				player.setCHealth(PC_HEALTH);
-				if (player.getCHealth() <= 0)
-					return false;
-				break;
-			case 2:
-				H.pln("You have blocked the enemy attack!");
-				break;
-			case 3:
-				H.pln("You have fled");
-				flee = true;
-				break;
-			}
+            switch(choice) {
+            case 1:
+                if(engine.nextInt(99)+1 <= 95) {
+                    damage = engine.nextInt(P_MAX_DAMAGE[plevel]);
+                    H.pln("Enemy lost "+damage+" health!");
+                    EC_HEALTH-=damage;
+                }
+                else {
+                    if(plevel<=8) {
+                        damage = engine.nextInt(P_MAX_DAMAGE[plevel+2]);
+                        H.pln("Critical Hit!");
+                        H.pln("Enemy lost "+damage+" health!");
+                    }
+                    else {
+                        if(plevel == 9) {
+                            damage = engine.nextInt(P_MAX_DAMAGE[plevel+1]);
+                            H.pln("Critical Hit!");
+                            H.pln("Enemy lost "+damage+" health!");
+                        }
+                        if(plevel == 10) {
+                            damage = EC_HEALTH;
+                            H.pln("You destroyed the Enemy with one blow!!!");
+                        }
+                    }
+                }
+                damage = engine.nextInt(E_MAX_DAMAGE[elevel])+1;
+                H.pln("You lost "+damage+" health");
+                PC_HEALTH-=damage;
+                player.setCHealth(PC_HEALTH);
+                if (player.getCHealth() <= 0)
+                    return false;
+                break;
+            case 2:
+                H.pln("You have blocked the enemy attack!");
+                break;
+            case 3:
+                H.pln("You have fled");
+                flee = true;
+                break;
+            }
 
-		}
-		if(EC_HEALTH <=0 && PC_HEALTH > 0){
-			player.addXP(((elevel)*100)-EC_HEALTH);
-			H.pln("You gained "+String.valueOf((((elevel)*100)-EC_HEALTH))+" XP!");
-		}
-		if(flee){
-			player.addXP((EC_HEALTH)*(-1));
-			H.pln("You lost "+EC_HEALTH+" XP!");
-		}
-		player.saveAll();
-		return true;
-	}
+        }
+        if(EC_HEALTH <=0 && PC_HEALTH > 0) {
+            player.addXP(((elevel)*100)-EC_HEALTH);
+            H.pln("You gained "+String.valueOf((((elevel)*100)-EC_HEALTH))+" XP!");
+        }
+        if(flee) {
+            player.addXP((EC_HEALTH)*(-1));
+            H.pln("You lost "+EC_HEALTH+" XP!");
+        }
+        player.saveAll();
+        return true;
+    }
 }
 
--- src/Location.java
+++ src/Location.java
@@ -10,189 +10,189 @@ import java.util.Random;
 
 public class Location {
 
-	private int x;
-	private int y;
-	private int maxX;
-	private int maxY;
-	private boolean alive;
-	private int mapSize;
-	private int[][] world;
-	private boolean hasMap;
-	private String playerName;
+    private int x;
+    private int y;
+    private int maxX;
+    private int maxY;
+    private boolean alive;
+    private int mapSize;
+    private int[][] world;
+    private boolean hasMap;
+    private String playerName;
 
-	public Location(int worldSize, String charName, int locationX, int locationY) throws IOException {
-		x = locationX;
-		y = locationY;
-		mapSize = worldSize;
-		maxX = mapSize - 1;
-		maxY = mapSize - 1;
-		world = new int[mapSize][mapSize];
-		playerName = charName;
-		genWorld();
-		//showMap();
-	}
+    public Location(int worldSize, String charName, int locationX, int locationY) throws IOException {
+        x = locationX;
+        y = locationY;
+        mapSize = worldSize;
+        maxX = mapSize - 1;
+        maxY = mapSize - 1;
+        world = new int[mapSize][mapSize];
+        playerName = charName;
+        genWorld();
+        //showMap();
+    }
 
-	public void move(int direction) {
-		switch (direction) {
+    public void move(int direction) {
+        switch (direction) {
 
-		case 1:
-			if(y<maxY){
-				y++;
-			}
-			else{
-				H.pln("You hae reached the limits of the island");
-			}
-			break;
-		case 2:
-			if(x<maxX){
-				x++;
-			}
-			else{
-				H.pln("You hae reached the limits of the island");
-			}
+        case 1:
+            if(y<maxY) {
+                y++;
+            }
+            else {
+                H.pln("You hae reached the limits of the island");
+            }
+            break;
+        case 2:
+            if(x<maxX) {
+                x++;
+            }
+            else {
+                H.pln("You hae reached the limits of the island");
+            }
 
-			break;
-		case 3:
-			if(y>0){
-				y--;
-			}
-			else{
-				H.pln("You have reached the limits of the island");
-			}
-			break;
-		case 4:
-			if(x>=0){
-				x--;
-			}
-			else{
-				H.pln("You have reached the limits of the island");
-			}
-			break;
-		default:
-			break;
-		}
-	}
-	public int getX() {
-		return x;
-	}
-	public int getY() {
-		return y;
-	}
-	public void died() {
-		x = mapSize/2;
-		y = 0;
-	}
-	/*
-	 * This method prints the map into the system output
-	 */
-	public void showMap() {
-		for(int r = 0; r < mapSize; r++) {
-			String str = "";
-			for(int f = 0; f < mapSize; f++) {
-				str = str + world[f][r];
-			}
-			System.out.println(str);
-		}
-	}
+            break;
+        case 3:
+            if(y>0) {
+                y--;
+            }
+            else {
+                H.pln("You have reached the limits of the island");
+            }
+            break;
+        case 4:
+            if(x>=0) {
+                x--;
+            }
+            else {
+                H.pln("You have reached the limits of the island");
+            }
+            break;
+        default:
+            break;
+        }
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void died() {
+        x = mapSize/2;
+        y = 0;
+    }
+    /*
+     * This method prints the map into the system output
+     */
+    public void showMap() {
+        for(int r = 0; r < mapSize; r++) {
+            String str = "";
+            for(int f = 0; f < mapSize; f++) {
+                str = str + world[f][r];
+            }
+            System.out.println(str);
+        }
+    }
 
-	public void editMap(int editX, int editY, int worldType) throws IOException {
-		world[editX][editY] = worldType;
-		File edit = new File(playerName+"Map.txt");
-		edit.delete();
-		edit.createNewFile();
-		BufferedWriter editMap = new BufferedWriter(new FileWriter(edit));
-		String str = "";
-		for(int a = 0; a < mapSize; a++) {
-			str = "";
-			for(int b = 0; b < mapSize; b++) {
-				str = str + Integer.toString(world[a][b]);
-			}
-			editMap.write(str);
-			editMap.newLine();
-		}
-		editMap.close();
-	}
+    public void editMap(int editX, int editY, int worldType) throws IOException {
+        world[editX][editY] = worldType;
+        File edit = new File(playerName+"Map.txt");
+        edit.delete();
+        edit.createNewFile();
+        BufferedWriter editMap = new BufferedWriter(new FileWriter(edit));
+        String str = "";
+        for(int a = 0; a < mapSize; a++) {
+            str = "";
+            for(int b = 0; b < mapSize; b++) {
+                str = str + Integer.toString(world[a][b]);
+            }
+            editMap.write(str);
+            editMap.newLine();
+        }
+        editMap.close();
+    }
 
-	public int getCell() {
-		return world[x][y];
-	}
+    public int getCell() {
+        return world[x][y];
+    }
 
-	/*
-	 * This method generates the world or if the player has a save file reloads the world.
-	 */
-	private void genWorld() throws IOException {
-		if(Exists(playerName)) {
-			BufferedReader load = new BufferedReader(new FileReader(playerName+"Map.txt"));
-			String loadMap[] = new String[mapSize];
-			for(int p = 0; p < mapSize; p++) {
-				loadMap[p] = load.readLine();
-			}
-			for(int a = 0; a < mapSize; a++) {
-				for(int b = 0; b < mapSize; b++) {
-					world[a][b] = Integer.parseInt(loadMap[a].substring(b, b+1));
-				}
-			}
-			load.close();
+    /*
+     * This method generates the world or if the player has a save file reloads the world.
+     */
+    private void genWorld() throws IOException {
+        if(Exists(playerName)) {
+            BufferedReader load = new BufferedReader(new FileReader(playerName+"Map.txt"));
+            String loadMap[] = new String[mapSize];
+            for(int p = 0; p < mapSize; p++) {
+                loadMap[p] = load.readLine();
+            }
+            for(int a = 0; a < mapSize; a++) {
+                for(int b = 0; b < mapSize; b++) {
+                    world[a][b] = Integer.parseInt(loadMap[a].substring(b, b+1));
+                }
+            }
+            load.close();
 
-		} else {
-			File newMap = new File(playerName+"Map.txt");
-			newMap.createNewFile();
+        } else {
+            File newMap = new File(playerName+"Map.txt");
+            newMap.createNewFile();
 
-			BufferedWriter map = new BufferedWriter(new FileWriter(newMap));
-			Random rand = new Random();
-			for(int i = 0; i < mapSize; i++) {
-				for(int j = 0; j < mapSize; j++) {
-					int randNum = rand.nextInt(100)+1;
-					int type = 0;
-					// Quicksand block
-					if(randNum > 0 && randNum < 11) type = 1;
-					// forest block
-					if(randNum > 10 && randNum <61) type = 2;
-					// pond block
-					if(randNum > 90 && randNum < 101) type = 3;
-					// field block
-					if(randNum > 65 && randNum < 91) type = 4;
-					//town block
-					if(randNum > 60 && randNum < 66) type = 7;
-					//type 8 is a player built house
-					if(type == 0) {
-						System.out.println("Land Generator Error");
-						System.out.println("Program Terminated");
-						System.exit(0);
-					}
-					world[j][i] = type;
-				}
+            BufferedWriter map = new BufferedWriter(new FileWriter(newMap));
+            Random rand = new Random();
+            for(int i = 0; i < mapSize; i++) {
+                for(int j = 0; j < mapSize; j++) {
+                    int randNum = rand.nextInt(100)+1;
+                    int type = 0;
+                    // Quicksand block
+                    if(randNum > 0 && randNum < 11) type = 1;
+                    // forest block
+                    if(randNum > 10 && randNum <61) type = 2;
+                    // pond block
+                    if(randNum > 90 && randNum < 101) type = 3;
+                    // field block
+                    if(randNum > 65 && randNum < 91) type = 4;
+                    //town block
+                    if(randNum > 60 && randNum < 66) type = 7;
+                    //type 8 is a player built house
+                    if(type == 0) {
+                        System.out.println("Land Generator Error");
+                        System.out.println("Program Terminated");
+                        System.exit(0);
+                    }
+                    world[j][i] = type;
+                }
 
-			}
-			for(int k = 0; k < mapSize; k++) {
-				world[k][0] = 5;
-				world[k][mapSize - 1] = 5;
-				world[0][k] = 5;
-				world[mapSize - 1][k] = 5;
-			}
+            }
+            for(int k = 0; k < mapSize; k++) {
+                world[k][0] = 5;
+                world[k][mapSize - 1] = 5;
+                world[0][k] = 5;
+                world[mapSize - 1][k] = 5;
+            }
 
-			int clx = rand.nextInt(mapSize);
-			int cly = rand.nextInt(mapSize);
+            int clx = rand.nextInt(mapSize);
+            int cly = rand.nextInt(mapSize);
 
-			//set castle location
-			world[clx][cly] = 6;
+            //set castle location
+            world[clx][cly] = 6;
 
-			String str = "";
-			for(int a = 0; a < mapSize; a++) {
-				str = "";
-				for(int b = 0; b < mapSize; b++) {
-					str = str + Integer.toString(world[a][b]);
-				}
-				map.write(str);
-				map.newLine();
-			}
-			map.close();
+            String str = "";
+            for(int a = 0; a < mapSize; a++) {
+                str = "";
+                for(int b = 0; b < mapSize; b++) {
+                    str = str + Integer.toString(world[a][b]);
+                }
+                map.write(str);
+                map.newLine();
+            }
+            map.close();
 
-		}
-	}
-	public boolean Exists(String charName){
-		File f = new File(charName+"Map.txt");
-		return f.exists();
-	}
+        }
+    }
+    public boolean Exists(String charName) {
+        File f = new File(charName+"Map.txt");
+        return f.exists();
+    }
 }
 
